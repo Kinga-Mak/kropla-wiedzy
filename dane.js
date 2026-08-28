972f75c7dac300bcdc09cc3355f571969951e991
Kinga1237 <kinga@makowska.hub.pl>
2026-08-28 16:15:10 +0200

    Nowe ciekawostki

--- dane.js
+++ dane.js
@@ -66,7 +66,7 @@ const bazaWiedzy = [
                 "Umaszczenie tygrysów uzależnione jest od pory roku i od podgatunku. Bywa od żółtej do czerwonopomarańczowej z wyraźnymi czarnymi pręgami.",
                 "Tylne nogi tygrysa są dłuższe od przednich, dzięki czemu jest on maszyną do biegania, skakania i atakowania.",
                 "Choć tygrys jest ekspertem w polowaniu, tylko jedno na 20 polowań kończy się zabiciem zwierzęcia. Na obszarach, gdzie zwierzyny jest niewiele, polowania kończą się sukcesem jeszcze rzadziej.",
-                
+
             ],
             en: [
                 "A tiger's roar is so loud that it can be heard from a distance of over 3 km.",
@@ -77,5 +77,69 @@ const bazaWiedzy = [
 
             ]
         }
-    }
+    },
+    {
+        id: "człowiek-ciało",
+        tytul: {
+            pl: "Ciało człowieka",
+            en: "Human body"
+        },
+        zdjecie: "https://images.unsplash.com/photo-1559757175-0828e13882ed?auto=format&fit=crop&w=600&q=80",
+        tagi: {
+            pl: ["człowiek", "ciało", "ssaki"],
+            en: ["human", "body", "mammals"],
+        },
+        ciekawostki: {
+            pl: [
+                "Większość komórek naszego ciała wielokrotnie ginie i rodzi się na nowo. Człowiek jest w stanie zmienić błonę wyściełającą jego żołądek średnio co 3 dni. Szkielet naszego ciała zmienia się co około 10 lat.",
+                "Serce człowieka jest na tyle silne, że mogłoby wypompować krew na przynajmniej 9 metrów w górę. Mięsień sercowy jest najsilniejszym mięśniem w naszym organizmie, ponieważ dostarcza krew do każdego zakątka ciała",
+                "W momencie, gdy regeneruje się skóra, wydłuża się nasze ciało. Poprzez zwiększoną pracę mózgu spalamy najwięcej kalorii.",
+                "Lekarze na całym świecie potwierdzają, że dziś rodzi się coraz więcej osób u których serce znajduje się po prawej stronie klatki piersiowej. Przypadłość ta nazywana jest Dekstrokardią",
+                "Żadne inny ssak na świecie nie ma tak wysokiej umieralności okołoporodowej jak człowiek.",
+                "Osoby o niebieskiej barwie oczu, są bardziej wrażliwi na ból. Mają za to możliwość spożywania większych ilości alkoholu",
+                "Rogówka jest jedynym fragmentem ludzkiego oka, które nie jest ukrwione. Tlen pobiera z atmosfery.",
+            ],
+            en: [
+                "Most of the cells in our body die and are born again multiple times. A person is able to change the lining of their stomach on average every 3 days. The skeleton of our body changes roughly every 10 years.",
+                "The human heart is strong enough that it could pump blood at least 9 meters up. The heart muscle is the strongest muscle in our body because it delivers blood to every corner of the body.",
+                "When the skin regenerates, our body lengthens. By working our brain more, we burn the most calories.",
+                "Doctors all over the world confirm that nowadays more and more people are being born with their heart on the right side of the chest. This condition is called dextrocardia.",
+                "No other mammal in the world has such a high perinatal mortality rate as humans.",
+                "People with blue eyes are more sensitive to pain. On the other hand, they can drink larger amounts of alcohol.",
+                "The cornea is the only part of the human eye that is not supplied with blood. It takes oxygen from the atmosphere.",
+            ]
+        }
+    },
+    {
+        id: "rośliny",
+        tytul: {
+            pl: "Rośliny",
+            en: "Plants"
+        },
+        zdjecie: "https://images.unsplash.com/photo-1712342109846-a8fcb1c883ba??auto=format&fit=crop&w=600&q=80",
+        tagi: {
+            pl: ["rosliny", "flora"],
+            en: ["plants", "floral"],
+        },
+        ciekawostki: {
+            pl: [
+                "Największe rośliny leśne produkują do 70 kg czystego tlenu rocznie. Filtrują do 100 000 metrów sześciennych powietrza.",
+                "Dąb jest gatunkiem drzewa najczęściej uderzanym przez pioruny. Miłorząb natomiast nie bywa trafiany przez pioruny.",
+                "Marchewki w oryginale są fioletowe – pomarańczowe zawdzięczany holenderskim Ogrodnikom.",
+                "Truskawka to jedyny owoc, którego nasiona znajdują się na zewnątrz, a nie w środku owocu.",
+                "Rośliny nie lubią hałasu. Ogranicza ich rozwój i pogarsza kondycję. Zatem nie bez powodu mówi się, że nie wolno się kłócić przy roślinach.",
+                "Według zaleceń dietetyków aby utrzymać organizm w zdrowiu należy spożywać 8 truskawek dziennie.",
+                "Rośliny rosnące w grupach, jak np. drzewa w lesie, często tworzą sieć korzeniową, która pozwala im dzielić się wodą i składnikami odżywczymi.",
+            ],
+            en: [
+                "The largest forest plants produce up to 70 kg of pure oxygen per year. They filter up to 100,000 cubic meters of air.",
+                "The oak is the tree species most often struck by lightning. Ginkgo, on the other hand, doesn’t usually get hit by lightning.",
+                "Carrots are originally purple – the orange ones are thanks to Dutch gardeners.",
+                "Truskawka to jedyny owoc, którego nasiona znajdują się na zewnątrz, a nie w środku owocu.",
+                "Plants don't like noise. It limits their growth and worsens their condition. So it's not without reason that people say you shouldn't argue around plants.",
+                "According to nutritionists, to keep your body healthy, you should eat 8 strawberries a day.",
+                "Plants that grow in groups, like trees in a forest, often form a root network that lets them share water and nutrients.",
+            ]
+        }
+    },
 ];
